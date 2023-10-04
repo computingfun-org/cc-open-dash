d6ee9192faf286a2d9ead5ddc6328b6d066fd86a
Zachary Cohen <github@computingfun.org>
2023-10-04 00:26:45 -0400

    v1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use cc_core::{JobNumber, JobURL};
+
 fn main() {
-    println!("Hello, world!");
+    let results = std::env::args()
+        .skip(1)
+        .map(|input| input.trim().parse::<JobNumber>())
+        .map(|input| input.map(|num| JobURL::from(num)))
+        .map(|input| input.map(|url| open::that(url.as_str()).map(|_| url)));
+
+    for result in results {
+        match result {
+            Ok(ok) => match ok {
+                Ok(ok) => println!("Opened: {}", ok),
+                Err(err) => eprintln!("Error: {}", err),
+            },
+            Err(err) => eprintln!("Error: {}", err),
+        }
+    }
 }

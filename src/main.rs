d602263aefc62df50c8d464eb33c73191c38a90f
Zachary Cohen <github@computingfun.org>
2023-10-05 22:34:26 -0400

    v2, add support for copied numbers

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 use cc_core::{JobNumber, JobURL};
 
 fn main() {
-    let results = std::env::args()
-        .skip(1)
-        .map(|args| args.trim().parse::<JobNumber>())
-        .map(|num| num.map(|num| JobURL::from(num)))
-        .map(|url| url.map(|url| open::that(url.as_str()).map(|_| url)));
+    let args = std::env::args().skip(1);
+    match args.len() == 0 {
+        true => open_jobs_from_clipboard(),
+        false => args.for_each(|s| open_jobs(&s)),
+    }
+}
 
-    for result in results {
-        match result {
-            Ok(parsed) => match parsed {
-                Ok(opened) => println!("Opened: {}", opened),
-                Err(opened_err) => eprintln!("Error: {}", opened_err),
-            },
-            Err(parsed_err) => eprintln!("Error: {}", parsed_err),
-        }
+fn open_jobs_from_clipboard() {
+    let clipboard = match cli_clipboard::get_contents() {
+        Ok(contents) => contents,
+        Err(err) => return eprintln!("{err}"),
+    };
+    open_jobs(clipboard.as_str());
+}
+
+fn open_jobs(job_nums: &str) {
+    job_nums.split_whitespace().for_each(open_job)
+}
+
+fn open_job(job_num: &str) {
+    use std::str::FromStr;
+    let url = match JobNumber::from_str(job_num) {
+        Ok(num) => JobURL::from(num),
+        Err(err) => return eprintln!("{err}"),
+    };
+    match open::that(url.as_str()) {
+        Ok(_) => println!("{url}"),
+        Err(err) => eprintln!("{err}"),
     }
 }
